d702242135d36b8885e9b81a3720bef796103b14
Mateusz Ziemiewicz <matt.ziemiewicz@gmail.com>
2022-07-21 14:56:39 +0200

    Initial commit

---
+++ ASCIIart/src/main/java/ASCIIart.java
@@ -0,0 +1,129 @@
+import java.io.File;
+import java.io.IOException;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.util.Scanner;
+
+// Best to lower font size in CLI to see the outcome
+
+public class ASCIIart {
+    public static void main(String[] user) {
+
+        Scanner input = new Scanner(System.in);
+        BufferedImage img = null;
+        File f;
+        String asciiCharacters = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$";
+
+        // read image
+        try {
+            f = new File("C:\\Users\\nokak\\Desktop\\hehexd\\ben_dover1.png");
+            img = ImageIO.read(f);
+            System.out.println("Successfully loaded image!");
+
+        } catch(IOException e) {
+            System.out.println(e);
+        }
+
+        // get height and width
+        assert img != null;
+        int height = img.getHeight();
+        int width = img.getWidth();
+
+        System.out.println("Image size: " + height + " x " + width);
+
+        System.out.println("1. Average\n2. MinMax\n3. Luminosity");
+
+        try {
+            int user1 = input.nextInt();
+            System.out.println(user1);
+            if (user1 == 1) {
+                Average(img, height, width, asciiCharacters);
+            } else if (user1 == 2) {
+                MinMax(img, height, width, asciiCharacters);
+            } else if (user1 == 3) {
+                Luminosity(img, height, width, asciiCharacters);
+            }
+            else {
+                System.exit(0);
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+    }
+
+    // function using average value of RGB
+    public static void Average(BufferedImage img, int height, int width, String ascii) {
+        for (int y = 0; y < height; y ++) {
+
+            for (int x = 0; x < width; x++) {
+
+                int p = img.getRGB(x, y);
+
+                // int a = (p>>24) & 0xff;
+                int r = (p>>16) & 0xff;
+                int g = (p>>8) & 0xff;
+                int b = p & 0xff;
+
+                // used in avg function
+                double avg = (r+g+b)/3;
+
+                double brightness = (avg/255)*ascii.length();
+                System.out.print(ascii.charAt((int)brightness));
+                System.out.print(ascii.charAt((int)brightness));
+                System.out.print(ascii.charAt((int)brightness));
+            }
+            System.out.println();
+        }
+    }
+
+    // function using min max
+    public static void MinMax(BufferedImage img, int height, int width, String ascii) {
+        for (int y = 0; y < height; y ++) {
+
+            for (int x = 0; x < width; x++) {
+
+                int p = img.getRGB(x, y);
+
+                // int a = (p>>24) & 0xff;
+                int r = (p>>16) & 0xff;
+                int g = (p>>8) & 0xff;
+                int b = p & 0xff;
+
+                int max = r>g ? (Math.max(r, b)) : (Math.max(g, b));
+                int min = r<g ? (Math.min(r, b)) : (Math.min(g, b));
+                double min_max = (max+min)/2;
+
+                double brightness = (min_max/255)*ascii.length();
+                System.out.print(ascii.charAt((int)brightness));
+                System.out.print(ascii.charAt((int)brightness));
+                System.out.print(ascii.charAt((int)brightness));
+            }
+            System.out.println();
+        }
+    }
+
+    // function using luminosity
+    public static void Luminosity(BufferedImage img, int height, int width, String ascii) {
+        for (int y = 0; y < height; y ++) {
+
+            for (int x = 0; x < width; x++) {
+
+                int p = img.getRGB(x, y);
+
+                // int a = (p>>24) & 0xff;
+                int r = (p>>16) & 0xff;
+                int g = (p>>8) & 0xff;
+                int b = p & 0xff;
+
+                double luminosity = 0.21 * r + 0.72 * g + 0.07 * b;
+
+                double brightness = (luminosity/255)*ascii.length();
+                System.out.print(ascii.charAt((int)brightness));
+                System.out.print(ascii.charAt((int)brightness));
+                System.out.print(ascii.charAt((int)brightness));
+            }
+            System.out.println();
+        }
+    }
+}
